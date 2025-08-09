9467f72a18f839245c2bd9a9a3012569441f4243
FranciscoJuliao27 <francisco.juliao.moura.amado@ccc.ufcg.edu.br>
2025-08-09 20:12:07 -0300

    Inicializando o EJS + landing page

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
+
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(process.cwd(), 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index', { titulo: 'Testando EJS' });
+});
+
+app.listen(3000, () => console.log(`Servidor aberto na porta 3000`));
