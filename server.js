9c93a0f270730b5731caf241889891108e29c528
FranciscoJuliao27 <francisco.juliao.moura.amado@ccc.ufcg.edu.br>
2025-08-09 21:19:00 -0300

    Aplicando EJS e gerando request via form

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(process.cwd(), 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index', { titulo: 'Testando EJS' });
+    res.render('index')
+});
+
+app.post('/bem-vindo', (req, res) =>{
+    const nome = req.body.nome;
+    res.render('bemvindo', { nome: nome })
 });
 
 app.listen(3000, () => console.log(`Servidor aberto na porta 3000`));
